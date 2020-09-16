3b4d78a40285a33ef7df2ce7ddcd1eaa60ee7d1b
alejor64 <alejoruizs@hotmail.com>
2020-09-15 21:57:55 -0500

    Adding docs folder

---
+++ docs/precache-manifest.07470a24504c5b00408e845a73b6acae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e39bf4d32940905ce8f36bd8a74ff9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51f3dfa7d9257a43d37f",
+    "url": "/static/js/2.16e21431.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.16e21431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa8408ca1a04f3f80886",
+    "url": "/static/js/main.94cfd8fb.chunk.js"
+  },
+  {
+    "revision": "5df29ddddcc6f70637bd",
+    "url": "/static/js/runtime-main.cd60aa92.js"
+  }
+]);
